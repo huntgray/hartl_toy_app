312a7afd613eb09194cea0e0d0795b63e5b10e2b
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-20 19:36:33 -0400

    updates route from 'index' to '/'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#toy_app'
-
-  get 'index', to: 'application#toy_app'
+  get '/', to: 'application#toy_app'
 
 end
